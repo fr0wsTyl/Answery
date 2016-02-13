55c63b6e9f584281e19018b76f22eabfc32d4742
fr0wsTyl <frowstyl@gmail.com>
2016-02-13 10:33:43 +0200

    Added basic template

---
+++ Source/Answery/Web/Answery.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Answery.Web.Startup))]
+namespace Answery.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
