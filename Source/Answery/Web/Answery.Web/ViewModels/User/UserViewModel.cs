54e968fc02613f0c40a4a287091bc2f0f6e360b4
fr0wsTyl <frowstyl@gmail.com>
2016-02-20 19:58:17 +0200

    Added user view model

---
+++ Source/Answery/Web/Answery.Web/ViewModels/User/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Answery.Web.ViewModels.User
+{
+    public class UserViewModel
+    {
+    }
+}
