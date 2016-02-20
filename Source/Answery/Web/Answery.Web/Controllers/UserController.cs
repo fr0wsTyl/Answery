27e2bf300c4d60dc5b9c4d231cb93318ff6bb6d1
fr0wsTyl <frowstyl@gmail.com>
2016-02-20 19:57:25 +0200

    Added user route. Added user controller and view

---
+++ Source/Answery/Web/Answery.Web/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿namespace Answery.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class UserController : Controller
+    {
+        public ActionResult Index(string username)
+        {
+            return View();
+        }
+    }
+}
