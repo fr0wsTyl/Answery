148faf17e47035245ed478e72ad8848d7ce532e1
fr0wsTyl <frowstyl@gmail.com>
2016-02-21 13:19:27 +0200

    Added requirements for the content of a question

--- Source/Answery/Data/Answery.Data.Models/Question.cs
+++ Source/Answery/Data/Answery.Data.Models/Question.cs
@@ -1,8 +1,12 @@
 ﻿namespace Answery.Data.Models
 {
+    using System.ComponentModel.DataAnnotations;
     using Common.Models;
     public class Question : BaseModel<int>
     {
+        [Required]
+        [MinLength(8)]
+        [MaxLength(600)]
         public string Content { get; set; }
 
         public bool IsAnswered { get; set; }
--- Source/Answery/Web/Answery.Web/ViewModels/Question/QuestionViewModel.cs
+++ Source/Answery/Web/Answery.Web/ViewModels/Question/QuestionViewModel.cs
@@ -1,9 +1,13 @@
 ﻿namespace Answery.Web.ViewModels.Question
 {
+    using System.ComponentModel.DataAnnotations;
     using Data.Models;
     using Infrastructure.Mapping.MvcTemplate.Web.Infrastructure.Mapping;
     public class QuestionViewModel : IMapFrom<Question>
     {
+        [Required]
+        [MinLength(8)]
+        [MaxLength(600)]
         public string Content { get; set; }
     }
 }
