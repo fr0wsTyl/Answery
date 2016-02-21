5b36d6ef7faeb8ce9fc9707a8c0d896d5d538f45
fr0wsTyl <frowstyl@gmail.com>
2016-02-21 13:36:47 +0200

    Added ajax for adding a question. Added modelstate validation in questions controller

--- Source/Answery/Web/Answery.Web/Controllers/QuestionsController.cs
+++ Source/Answery/Web/Answery.Web/Controllers/QuestionsController.cs
@@ -4,17 +4,33 @@
     using System.Web.Mvc;
     using Data.Models;
     using Infrastructure.Mapping;
+    using Services.Interfaces;
     using ViewModels.Question;
     using ViewModels.User;
 
     public class QuestionsController : Controller
     {
+        private readonly IQuestionsService questionsService;
+
+        public QuestionsController(IQuestionsService questionsService)
+        {
+            this.questionsService = questionsService;
+        }
+
         [HttpPost]
         public JsonResult Add(QuestionViewModel questionInput)
         {
-            var question = AutoMapperConfig.Configuration.CreateMapper().Map<QuestionViewModel, Question>(questionInput);
-
-            return System.Web.Helpers.Json();
+            if (ModelState.IsValid)
+            {
+                var question =
+                    AutoMapperConfig.Configuration.CreateMapper().Map<QuestionViewModel, Question>(questionInput);
+                var questionAdded = this.questionsService.Add(question);
+                return Json(questionAdded);
+            }
+            else
+            {
+                return Json(new { message = "Invalid data"});
+            }
         }
     }
 }
