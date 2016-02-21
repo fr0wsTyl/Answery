ef023f4a80c7f37f38d78318301499e2aebeab44
fr0wsTyl <frowstyl@gmail.com>
2016-02-21 19:24:42 +0200

    Added comment view model

---
+++ Source/Answery/Web/Answery.Web/ViewModels/Comment/CommentViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Answery.Web.ViewModels.Comment
+{
+    using System.ComponentModel.DataAnnotations;
+    using Data.Models;
+    using Question;
+    using User;
+
+    public class CommentViewModel
+    {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(300)]
+        public string Content { get; set; }
+
+        public string QuestionId { get; set; }
+
+        public virtual QuestionViewModel Question { get; set; }
+
+        public virtual string AuthorId { get; set; }
+
+        public virtual UserViewModel Author { get; set; }
+    }
+}
