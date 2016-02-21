897f87944630f276e62b79b594b91b9022ba1e27
fr0wsTyl <frowstyl@gmail.com>
2016-02-21 19:23:19 +0200

    Added comment model

--- Source/Answery/Data/Answery.Data.Models/Comment.cs
+++ Source/Answery/Data/Answery.Data.Models/Comment.cs
@@ -2,23 +2,19 @@
 {
     using System.ComponentModel.DataAnnotations;
     using Common.Models;
-    public class Question : BaseModel<int>
+    public class Comment : BaseModel<int>
     {
         [Required]
         [MinLength(5)]
-        [MaxLength(600)]
+        [MaxLength(300)]
         public string Content { get; set; }
 
-        public bool IsAnswered { get; set; }
+        public string QuestionId { get; set; }
 
-        public string ReceiverId { get; set; }
-
-        public virtual User Receiver { get; set; }
+        public virtual Question Question { get; set; }
 
         public virtual string AuthorId { get; set; }
 
         public virtual User Author { get; set; }
-
-
     }
 }
