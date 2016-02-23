790d6c26ba12fe8b6fb770be5d2962f062087b4f
fr0wsTyl <frowstyl@gmail.com>
2016-02-23 21:33:33 +0200

    Added question answer view model

--- Source/Answery/Web/Answery.Web/Controllers/AnswerController.cs
+++ Source/Answery/Web/Answery.Web/Controllers/AnswerController.cs
@@ -1,12 +1,43 @@
 ﻿namespace Answery.Web.Controllers
 {
     using System.Web.Mvc;
+    using Services.Interfaces;
 
     public class AnswerController : Controller
     {
-        public AnswerController()
+        private readonly IAnswersService answersService;
+
+        public AnswerController(IAnswersService answersService)
         {
-            
+            this.answersService = answersService;
         }
-    }
+
+        [HttpPost]
+        public JsonResult Add(QuestionAnswerViewModel questionInput)
+        {
+            if (ModelState.IsValid)
+            {
+                var question =
+                    AutoMapperConfig.Configuration.CreateMapper().Map<QuestionViewModel, Question>(questionInput);
+                if (question.AuthorId != null)
+                {
+                    question.Author = this.usersService.GetUserById(question.AuthorId);
+                }
+
+                question.Receiver = this.usersService.GetUserById(question.ReceiverId);
+
+                var questionAdded = this.questionsService.Add(question);
+
+                //Checking if the question is added successfully
+                if (questionAdded.IsAnswered == false)
+                {
+                    return Json(new { isSuccessfulAdded = true });
+                }
+                return Json(new { isSuccessfulAdded = false });
+            }
+            else
+            {
+                return Json(new { isSuccessfulAdded = false });
+            }
+        }
 }
---
+++ Source/Answery/Web/Answery.Web/ViewModels/Question/QuestionAnswerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Answery.Web.ViewModels.Question
+{
+    public class QuestionAnswerViewModel
+    {
+        public int QuestionId { get; set; }
+        public string Answer { get; set; }
+    }
+}
