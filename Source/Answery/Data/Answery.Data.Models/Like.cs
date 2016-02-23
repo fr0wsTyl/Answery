d6f391c674abfaf447fefbb8b4a05d4627ca3cae
fr0wsTyl <frowstyl@gmail.com>
2016-02-24 01:58:17 +0200

    Added like view model

---
+++ Source/Answery/Data/Answery.Data.Models/Like.cs
@@ -0,0 +1,15 @@
+﻿namespace Answery.Data.Models
+{
+    using System;
+    using Common.Models;
+    public class Like : BaseModel<int>
+    {
+        public string LikedByUserId { get; set; }
+
+        public User LikedByUser { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public virtual Question Question { get; set; }
+    }
+}
---
+++ Source/Answery/Web/Answery.Web/ViewModels/Like/LikeViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Answery.Web.ViewModels.Like
+{
+    using System;
+    using Question;
+    using User;
+
+    public class LikeViewModel
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+        public string LikedByUserId { get; set; }
+
+        public UserViewModel LikedByUser { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public virtual QuestionViewModel Question { get; set; }
+    }
+}
